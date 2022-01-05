a0dbb77faf2c014a10c13fd4b13ab17fe52b5af0
Adriana Aparicio Marijuán <adriana.aparicio@frontiersin.net>
2022-01-05 12:34:42 +0100

    fill process_data script save the data to the database after running script add requirements file with the necessary packages add some format rules for flake8 package

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,72 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load Messages Data with Categories Function
+
+    Arguments:
+        messages_filepath -> Path to the CSV file containing messages
+        categories_filepath -> Path to the CSV file containing categories
+    Output:
+        df -> Combined data containing messages and categories
+    """
+
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, on="id")
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Clean Data Function
+
+    Arguments:
+        df -> Combined data containing messages and categories
+    Outputs:
+        df -> Combined data containing messages and categories with categories cleaned up
+    """
+
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(pat=";", expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[[1]]
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = [category_name.split("-")[0] for category_name in row.values[0]]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    # drop the original categories column from `df`
+    df.drop(["categories"], axis=1, inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    cleaned_df = pd.concat([df, categories], join="inner", axis=1)
+    return cleaned_df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save Data to SQLite Database Function
+
+    Arguments:
+        df -> Combined data containing messages and categories with categories cleaned up
+        database_filename -> Path to SQLite destination database
+    """
+
+    engine = create_engine("sqlite:///" + database_filename)
+    table_name = database_filename.replace(".db", "")
+    df.to_sql(table_name, engine, index=False, if_exists="replace")
 
 
 def main():
@@ -18,26 +74,31 @@ def main():
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print(
+            "Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}".format(
+                messages_filepath, categories_filepath
+            )
+        )
         df = load_data(messages_filepath, categories_filepath)
 
-        print('Cleaning data...')
+        print("Cleaning data...")
         df = clean_data(df)
-        
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+
+        print("Saving data...\n    DATABASE: {}".format(database_filepath))
         save_data(df, database_filepath)
-        
-        print('Cleaned data saved to database!')
-    
+
+        print("Cleaned data saved to database!")
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+        print(
+            "Please provide the filepaths of the messages and categories "
+            "datasets as the first and second argument respectively, as "
+            "well as the filepath of the database to save the cleaned data "
+            "to as the third argument. \n\nExample: python process_data.py "
+            "disaster_messages.csv disaster_categories.csv "
+            "DisasterResponse.db"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
